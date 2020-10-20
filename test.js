c0659eae5a07cdc9df84995e9a381eed4cf6926a
Bokassa Jean le Grand <j_dabok@me.com>
2020-10-21 00:33:38 +0200

    Indroduction to Node server

--- test.js
+++ test.js
@@ -1,15 +1,34 @@
-let http = require("http")
+let express = require("express")
+let ourApp = express()
 
-let ourApp = http.createServer(function (req, res){
-    if (req.url == "/") {
-        res.end("Hello and welcome to our site")
+ourApp.use(express.urlencoded({extended: false}))
+
+ourApp.get('/', function (req, res) {
+    res.send(`
+    <form action="/answer" method="post">
+    <p>What color is the sky on a clear and sunny day ?</p>
+    <input name="skyColor" autocomplete="off">
+    <button>Submit Answer</button>
+    </form>
+    `)
+})
+
+ourApp.post('/answer', function (req, res) {
+    if (req.body.skyColor.toUpperCase() == "BLUE") {
+        res.send(` 
+        <p>Congrat is the coorest answer!</p>
+        <a href="/">Back to homepage</a>
+        `)
+    } else {
+        res.send(` 
+        <p>Sorry that is incorrect.</p>
+        <a href="/">Back to homepage</a>
+        `)
     }
-    if (req.url == "/about") {
-        res.end("Thank you for the interest in our campany")
-    }
-    if (req.url != "/" && req.url != "/about") {
-        res.end("Whoops, we cannot find that page.")
-    }
+})
+
+ourApp.get('/answer', function (req, res) {
+    res.send("Are you lost? There is nothing to see here.")
 })
 
 ourApp.listen(3000)
